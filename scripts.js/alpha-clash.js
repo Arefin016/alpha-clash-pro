19e884692dc752ae09446df3189c7bd1371cb701
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2024-02-15 01:17:20 +0600

    final project done

--- scripts.js/alpha-clash.js
+++ scripts.js/alpha-clash.js
@@ -13,6 +13,12 @@
  
 function handleKeyBoardKeyUpEvent(event){
     const playerPressed = event.key;
+    console.log( 'player pressed', playerPressed);
+
+    //stop the game if pressed 'ESC'
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
 
     // key player is expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
@@ -62,6 +68,10 @@ function handleKeyBoardKeyUpEvent(event){
         const updatedLife = currentLife - 1
         setTextElementValueById('current-life', updatedLife);
 
+        if(updatedLife === 0){
+            gameOver();
+        }
+
 
 
 
@@ -98,7 +108,38 @@ function continueGame(){
 }
 
 function play(){
+
+    //hide everything show only the playground
+
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    //reset score and life
+
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
+
+
+
     continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    //update final score
+    // 1.get the final score
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score', lastScore);
+
+
+    //clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+
+    removeBackgroundColorById(currentAlphabet);
 }
